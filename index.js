6fab5afea2cbc4a7c24bc662a72047d9332a0bee
Auryn D <46653341+inglowdev@users.noreply.github.com>
2020-10-19 18:23:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+
+client.on('ready', () => {
+	console.log("Ready.")
+
+	setInterval(() => {
+		const statuses = [
+			`HCReaper.Club`,
+			`Watching over HCReaper`,
+		]
+
+		const status = statuses[Math.floor(Math.random() * statuses.length)]
+		client.user.setActivity(status, { type: "PLAYING" })
+	}, 3000)
+
+})
+
+client.on('guildMemberAdd', member => {
+
+	const channel = member.guild.channels.cache.find(channel => channel.name === "arrivals");
+	if (!channel) return;
+
+	channel.send(`Welcome to HCReaper ${member}!
+    :star: hcreaper.club
+    :shopping_cart: store.hcreaper.club
+    
+    **Thanks For Joining!**`)
+
+
+});
+
+client.login('NzY3ODcyMTI5MDM4OTQyMjQ5.X44OpQ.2GJlHwFJD5MZcvNfj-qIUkxFcIY');
